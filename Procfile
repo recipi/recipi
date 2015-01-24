0f0d466df0f264f32480540c3d6967cd3b2d82d8
Christopher Grebs <cg@webshox.org>
2015-01-24 09:05:26 +0100

    Switch to gulp, to run everything use: 'gulp serve'. Livereload etc works just fine.

--- Procfile
+++
@@ -1,4 +0,0 @@
-web: env PYTHONUNBUFFERED=true python manage.py runserver
-worker: env PYTHONUNBUFFERED=true celery worker -A recipi.core.tasks -l INFO -E --autoreload -B
-grunt: grunt serve
-
---
+++ gulpfile.js
@@ -0,0 +1,115 @@
+var gulp = require('gulp'),
+	del = require('del'),
+	plugins = require('gulp-load-plugins')(),
+	livereload = require('gulp-livereload'),
+	gutil = require('gulp-util'),
+	source = require('vinyl-source-stream'),
+	buffer = require('vinyl-buffer'),
+	browserify = require('browserify'),
+	to5ify = require('6to5ify'),
+	uglify = require('gulp-uglify'),
+	read = require('fs').readFileSync,
+	base = './src/recipi',
+	src = {
+		foundation: 'bower_components/foundation/scss',
+		templates: base + '/templates',
+		scss: base + '/static/scss',
+		fonts: base + '/static/fonts',
+		images: base + '/static/img',
+		js: base + '/static/js',
+		manageScript: 'manage.py'
+	},
+	dest = {
+		css: base + '/static/css',
+		js: base + '/static/js/build'
+	};
+
+/**
+ * Cleaning css directory
+ */
+gulp.task('clean:css', function (done) {
+	del(dest.css, done);
+});
+
+/**
+ * Cleaning js directory
+ */
+gulp.task('clean:js', function (done) {
+	del(dest.js, done);
+});
+
+/**
+ * Compiling sass/scss files to css
+ */
+gulp.task('scss', ['clean:css'], function () {
+	return gulp.src(src.scss + '/*.scss')
+		.pipe(plugins.plumber())
+		.pipe(plugins.sourcemaps.init())
+		.pipe(plugins.sass({
+			includePaths: [src.foundation]
+		}))
+		.pipe(plugins.sourcemaps.write('./'))
+		.pipe(gulp.dest(dest.css))
+		.pipe(livereload());
+});
+
+/**
+* Validating scss files
+*/
+gulp.task('validate:scss', function () {
+	return gulp.src(src.scss + '/**/*.scss')
+		.pipe(plugins.plumber())
+		.pipe(plugins.lintspaces())
+		.pipe(plugins.lintspaces.reporter());
+});
+
+/**
+* Validating js files
+*/
+gulp.task('validate:js', function () {
+	return gulp.src(src.js + '/*.js')
+		.pipe(plugins.plumber())
+		.pipe(plugins.jscs({
+			preset: 'jquery'
+		}))
+		.pipe(plugins.jshint())
+		.pipe(plugins.lintspaces())
+		.pipe(plugins.lintspaces.reporter());
+});
+
+/**
+* Building our javascript files
+*/
+gulp.task('js', function() {
+	browserify(src.js + '/src/app.js', {debug: true})
+		.transform(to5ify)
+		.bundle()
+		.on('error', gutil.log.bind(gutil, 'Browserify Error'))
+		.pipe(source('bundle.js'))
+		.pipe(buffer())
+		.pipe(plugins.sourcemaps.init({loadMaps: true})) // loads map from browserify file
+		.pipe(uglify())
+		.pipe(plugins.sourcemaps.write('./')) // writes .map file
+		.pipe(gulp.dest(src.js + '/build'))
+		.pipe(livereload());
+});
+
+/**
+* Validating files
+*/
+gulp.task('validate', ['validate:scss', 'validate:js']);
+
+gulp.task('watch', function () {
+	gulp.watch(src.js + '/src/*.js', ['validate:js', 'js']);
+	gulp.watch(src.scss + '/**/*.scss', ['validate:scss', 'scss']);
+	livereload.listen();
+});
+
+gulp.task('build', ['validate', 'scss']);
+
+gulp.task('default', ['build']);
+
+gulp.task('serve', ['default', 'watch'], plugins.shell.task([
+	'env PYTHONUNBUFFERED=true python ' + src.manageScript + ' runserver',
+	'env PYTHONUNBUFFERED=true celery worker -A recipi.core.tasks -l INFO -E',
+]));
