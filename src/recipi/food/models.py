186bd286a674a932223b56ba67e8714cf8348cca
Christopher Grebs <cg@webshox.org>
2015-01-25 17:41:28 +0100

    Making pep8 happy.

--- src/recipi/food/models.py
+++ src/recipi/food/models.py
@@ -15,7 +15,7 @@ from recipi.utils.db.uuid import UUIDField
 # The original database is not well designed so there are other ways
 # of structuring the data. Figure it out! (cg)
 
-### General information
+# General information
 # TODO: Write summary and move somewhere else!
 # by the True Retention Method (%TR) (Murphy et al., 1975). This method, as shown
 # below, accounts for the loss or gain of moisture and the loss of nutrients due to heat or
@@ -38,6 +38,7 @@ from recipi.utils.db.uuid import UUIDField
 # N = nutrient value per household measure,
 # V = nutrient value per 100 g (Nutr_Val in the Nutrient Data file), and
 # W = g weight of portion (Gm_Wgt in the Weight file).
+
 
 class FoodGroup(models.Model):
     id = UUIDField(auto=True, primary_key=True)
@@ -104,7 +105,8 @@ class FoodDescription(models.Model):
 
     # TODO: Write down summary of those paragraphs:
 
-    # The general factor of 6.25 is used to calculate protein in items that do not have a specific factor.
+    # The general factor of 6.25 is used to calculate protein in items that do not
+    # have a specific factor.
     # Protein values for chocolate, cocoa, coffee, mushrooms, and yeast were adjusted for
     # non-protein nitrogenous material (Merrill and Watt, 1973). The adjusted protein
     # conversion factors used to calculate protein for these items are as follows:
@@ -131,9 +133,9 @@ class FoodDescription(models.Model):
     # in yield, as appropriate. For multi-ingredient processed foods, if the kilocalories
     # calculated by the manufacturer are reported, no calorie factors are given.
 
-    # Calorie factors for fructose and sorbitol, not available in the Atwater system, are derived
-    # from the work of Livesay and Marinos (1988). Calorie factors for coffee and tea are
-    # estimated from those for seeds and vegetables, respectively.
+    # Calorie factors for fructose and sorbitol, not available in the Atwater system,
+    # are derived from the work of Livesay and Marinos (1988). Calorie factors
+    # for coffee and tea are estimated from those for seeds and vegetables, respectively.
 
     # Factor for converting nitrogen to protein (see above)
     nitrogen_factor = models.DecimalField(max_digits=4, decimal_places=2, default=0.0)
