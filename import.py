b8ed04f589277acc38b0bd36a1d0faecbf01e343
Christopher Grebs <cg@webshox.org>
2015-02-04 07:09:50 +0100

    remove import_recipes

--- import.py
+++ import.py
@@ -294,35 +294,5 @@ def import_usda(basepath, verbose=True):
     print('\n\n'.join(summary))
 
 
-
-def import_recipes(fname):
-    with codecs.open(fname, encoding='utf-8') as fobj:
-        raw_data = fobj.read()
-
-    recipes = []
-
-    for line in raw_data.splitlines():
-
-        try:
-            parsed = json.loads(line.strip())
-        except Exception:
-            continue
-
-        recipes.append({
-            'name': parsed['name'],
-            'source': parsed['source'],
-            'url': parsed['url'],
-            'recipe_yield': parsed.get('recipeYield', None),
-            'ingredients': parsed['ingredients'].split('\n'),
-            'prep_time': parsed.get('prepTime', None),
-            'cook_time': parsed.get('cookTime', None),
-            'published': parsed.get('datePublished', None),
-            'description': parsed.get('description', None),
-            'image': parsed.get('image', None)
-        })
-
-    return recipes
-
-
 if __name__ == '__main__':
     import_usda('./resources/usda/')
