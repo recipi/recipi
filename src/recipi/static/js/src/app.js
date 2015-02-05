22cd85fabc8f184b9242d77fe6c49143516d4ea9
Christopher Grebs <cg@webshox.org>
2015-02-05 07:58:47 +0100

    More editor modifications

--- src/recipi/static/js/src/app.js
+++ src/recipi/static/js/src/app.js
@@ -3,7 +3,7 @@ import Backbone from 'backbone';
 import Foundation from 'foundation';
 import brace from 'brace';
 import * as markdown from 'brace/mode/markdown';
-import * as monokai from 'brace/theme/monokai';
+import * as monokai from 'brace/theme/kuroir';
 
 $(() => {
 	Backbone.$ = $;
@@ -13,9 +13,20 @@ $(() => {
 
     var editor = ace.edit('editor');
 
-    editor.setTheme('ace/theme/monokai');
+    editor.setTheme('ace/theme/kuroir');
     editor.getSession().setMode('ace/mode/markdown');
     editor.setAutoScrollEditorIntoView(true);
+    editor.getSession().setUseWrapMode(true);
+    editor.getSession().setTabSize(4);
+    editor.getSession().setUseSoftTabs(true);
+
+    editor.setBehavioursEnabled(true);
+    editor.setHighlightSelectedWord(true);
+    editor.setHighlightActiveLine(true);
+    editor.setDisplayIndentGuides(true);
+    editor.setShowPrintMargin(false);
+    editor.setFontSize(16);
+
     editor.setOption('maxLines', 150);
 
     console.log("initialized!", editor);
