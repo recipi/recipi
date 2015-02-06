aab233f6acdf3634d11a6a65d75eaea98793c125
Christopher Grebs <cg@webshox.org>
2015-02-06 19:14:47 +0100

    Implement proper watchify support

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var gulp = require('gulp'),
 	browserify = require('browserify'),
 	to5ify = require('6to5ify'),
 	uglify = require('gulp-uglify'),
+	watchify = require('watchify'),
 	read = require('fs').readFileSync,
 	base = './src/recipi',
 	src = {
@@ -27,31 +28,31 @@ var gulp = require('gulp'),
 /**
  * Cleaning css directory
  */
-gulp.task('clean:css', function (done) {
-	del(dest.css, done);
-});
+ gulp.task('clean:css', function (done) {
+ 	del(dest.css, done);
+ });
 
 /**
  * Cleaning js directory
  */
-gulp.task('clean:js', function (done) {
-	del(dest.js, done);
-});
+ gulp.task('clean:js', function (done) {
+ 	del(dest.js, done);
+ });
 
 /**
  * Compiling sass/scss files to css
  */
-gulp.task('scss', ['clean:css'], function () {
-	return gulp.src(src.scss + '/*.scss')
-		.pipe(plugins.plumber())
-		.pipe(plugins.sourcemaps.init())
-		.pipe(plugins.sass({
-			includePaths: [src.foundation]
-		}))
-		.pipe(plugins.sourcemaps.write('./'))
-		.pipe(gulp.dest(dest.css))
-		.pipe(livereload());
-});
+ gulp.task('scss', ['clean:css'], function () {
+ 	return gulp.src(src.scss + '/*.scss')
+	 	.pipe(plugins.plumber())
+	 	.pipe(plugins.sourcemaps.init())
+	 	.pipe(plugins.sass({
+	 		includePaths: [src.foundation]
+	 	}))
+	 	.pipe(plugins.sourcemaps.write('./'))
+	 	.pipe(gulp.dest(dest.css))
+	 	.pipe(livereload());
+ });
 
 /**
 * Validating scss files
@@ -81,18 +82,36 @@ gulp.task('validate:js', function () {
 * Building our javascript files
 */
 gulp.task('js', function() {
-	browserify(src.js + '/src/app.js', {debug: true})
+	browserify(src.js + '/src/app.js', watchify.args)
 		.transform(to5ify)
 		.bundle({debug: true})
 		.on('error', gutil.log.bind(gutil, 'Browserify Error'))
 		.pipe(source('bundle.js'))
 		.pipe(buffer())
-		.pipe(plugins.sourcemaps.init({loadMaps: true})) // loads map from browserify file
+		.pipe(plugins.sourcemaps.init({loadMaps: true}))
 		.pipe(uglify())
-		.pipe(plugins.sourcemaps.write('./')) // writes .map file
+		.pipe(plugins.sourcemaps.write('./'))
 		.pipe(gulp.dest(src.js + '/build'))
 		.pipe(livereload());
 });
+
+
+var bundler = watchify(browserify(src.js + '/src/app.js', watchify.args));
+
+bundler.transform(to5ify);
+
+gulp.task('jjs', bundle);
+bundler.on('update', bundle);
+
+
+function bundle() {
+	return bundler.bundle()
+		.on('error', gutil.log.bind(gutil, 'Browserify Error'))
+		.pipe(source('bundle.js'))
+		.pipe(gulp.dest(src.js + '/build'))
+		.pipe(livereload());
+}
+
 
 /**
 * Validating files
@@ -100,7 +119,7 @@ gulp.task('js', function() {
 gulp.task('validate', ['validate:scss', 'validate:js']);
 
 gulp.task('watch', function () {
-	gulp.watch(src.js + '/src/*.js', ['validate:js', 'js']);
+	gulp.watch(src.js + '/src/*.js', ['validate:js', 'jjs']);
 	gulp.watch(src.scss + '/**/*.scss', ['validate:scss', 'scss']);
 	livereload.listen();
 });
@@ -109,7 +128,8 @@ gulp.task('build', ['validate', 'js', 'scss']);
 
 gulp.task('default', ['build']);
 
-gulp.task('serve', ['default', 'watch'], plugins.shell.task([
+gulp.task('serve', ['validate', 'scss', 'jjs', 'watch'], plugins.shell.task([
 	'env PYTHONUNBUFFERED=true python ' + src.manageScript + ' runserver',
 	'env PYTHONUNBUFFERED=true celery worker -A recipi.core.tasks -l INFO -E',
-]));
+	])
+);
