a1853e7ce6b07f4331461dd2371165a67868558c
Christopher Grebs <cg@webshox.org>
2015-02-06 17:29:10 +0100

    Update ace editor initialization

--- src/recipi/static/js/src/app.js
+++ src/recipi/static/js/src/app.js
@@ -1,4 +1,4 @@
-import jquery from 'jquery';
+import $ from 'jquery';
 import Backbone from 'backbone';
 import Foundation from 'foundation';
 import brace from 'brace';
@@ -6,28 +6,34 @@ import * as markdown from 'brace/mode/markdown';
 import * as monokai from 'brace/theme/kuroir';
 
 $(() => {
+    console.log('Initialize backbone...');
+
 	Backbone.$ = $;
 	Backbone.history.start({pushState: true});
 
+    console.log('Initialize foundation...');
 	Foundation.init();
 
-    var editor = ace.edit('editor');
+    if ($('.editor').length > 0) {
+        console.log('Initialize ACE editor...');
+        var editor = ace.edit($('.editor')[0]);
 
-    editor.setTheme('ace/theme/kuroir');
-    editor.getSession().setMode('ace/mode/markdown');
-    editor.setAutoScrollEditorIntoView(true);
-    editor.getSession().setUseWrapMode(true);
-    editor.getSession().setTabSize(4);
-    editor.getSession().setUseSoftTabs(true);
+        editor.setTheme('ace/theme/kuroir');
+        editor.getSession().setMode('ace/mode/markdown');
+        editor.setAutoScrollEditorIntoView(true);
+        editor.getSession().setUseWrapMode(true);
+        editor.getSession().setTabSize(4);
+        editor.getSession().setUseSoftTabs(true);
 
-    editor.setBehavioursEnabled(true);
-    editor.setHighlightSelectedWord(true);
-    editor.setHighlightActiveLine(true);
-    editor.setDisplayIndentGuides(true);
-    editor.setShowPrintMargin(false);
-    editor.setFontSize(16);
+        editor.setBehavioursEnabled(true);
+        editor.setHighlightSelectedWord(true);
+        editor.setHighlightActiveLine(true);
+        editor.setDisplayIndentGuides(true);
+        editor.setShowPrintMargin(false);
+        editor.setFontSize(16);
 
-    editor.setOption('maxLines', 150);
+        editor.setOption('maxLines', 150);
+    }
 
-    console.log("initialized!", editor);
+    console.log('Initialized!');
 });
